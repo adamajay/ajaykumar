6bd8690adcb16e3f9133df5f3b07ff67d15a1dd7
adamajay <30343197+adamajay@users.noreply.github.com>
2017-07-21 17:54:47 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("the number is positive");
+}
+elseif(n<0)
+{
+printf("the number is negative");
+}
+else(n=0)
+{
+printf("the number is zero");
+}
+}
